4f894ed0f8038d196744067fde0119a25b959651
Pritam Sonavne <pritamsonavne@gmail.com>
2025-05-11 21:24:57 -0500

    Complete AIChatbot functionality

---
+++ aichatbot/src/test/java/com/pri/ai/aichatbot/AichatbotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pri.ai.aichatbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AichatbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
